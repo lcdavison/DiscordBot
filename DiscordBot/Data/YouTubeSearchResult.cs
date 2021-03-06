19f154d9a110929ccdadc7c5ccc5d8174659d8f9
Luke Davison <luke.davison@talktalk.net>
2021-03-06 13:26:20 +0000

    Started implementing play command

---
+++ DiscordBot/Data/YouTubeSearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Data
+{
+    internal static class YouTubeSearchResult
+    {
+        public static List<string> Videos { get; set; } = new List<string>();
+    }
+}
---
+++ DiscordBot/Modules/MusicModule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using static DiscordBot.Data.YouTubeSearchResult;
+
+namespace DiscordBot.Modules
+{
+    public class MusicModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("play")]
+        public async Task PlayCommand([Remainder] string input)
+        {
+            if (Videos.Count == 0)
+            {
+                await PerformYouTubeSearch(input);
+
+                await Context.Channel.SendMessageAsync(
+                $"1 : {Videos[0]}\n" +
+                $"2 : {Videos[1]}\n" +
+                $"3 : {Videos[2]}\n" +
+                $"4 : {Videos[3]}\n" +
+                $"5 : {Videos[4]}");
+            }
+            else
+            {
+                int videoIndex = int.Parse(input);
+                videoIndex -= 1;
+
+                Console.WriteLine($"{Videos[videoIndex]}");
+
+                Videos.Clear();
+            }
+
+            Console.WriteLine(Videos.Count);
+        }
+
+        private async Task PerformYouTubeSearch(string keyword)
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyDEKL8yzjWd83TclQVAx5zHVfjaeNLx3Wo",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchRequest = youtubeService.Search.List("snippet");
+            searchRequest.Q = keyword;
+            searchRequest.MaxResults = 5;
+
+            var searchResponse = await searchRequest.ExecuteAsync();
+
+            foreach (var searchItem in searchResponse.Items)
+            {
+                switch (searchItem.Id.Kind)
+                {
+                    case "youtube#video":
+                        Videos.Add($"{searchItem.Snippet.Title}");
+                        break;
+                }
+            }
+        }
+    }
+}
